c49287f84b999c7b66e6ff4e5f4de33118043860
IlyaVostrukhin <i.s.vostrukhin@yandex.ru>
2022-09-08 18:14:45 +0700

    Added planner-utils, created LoggingAspect

---
+++ src/main/java/com/projects/planner/utils/aop/LoggingAspect.java
@@ -0,0 +1,37 @@
+package com.projects.planner.utils.aop;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import java.util.Calendar;
+
+@Aspect
+@Component
+@Log4j2
+public class LoggingAspect {
+
+    @Around("execution(* com.projects.planner.todo.controller..*(..)))")
+    public Object profileControllerMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        log.info("-----    Executing " + className + "." + methodName + ": " + Calendar.getInstance().getTime() + "   -----");
+
+        StopWatch countdown = new StopWatch();
+        countdown.start();
+        Object result = proceedingJoinPoint.proceed();
+        countdown.stop();
+
+        log.info("-----    Execution time of " + className + "." + methodName + ": " + countdown.getTotalTimeMillis() + "ms   -----");
+
+        return result;
+    }
+
+}
---
+++ src/test/java/com/projects/planner/utils/PlannerUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.planner.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
